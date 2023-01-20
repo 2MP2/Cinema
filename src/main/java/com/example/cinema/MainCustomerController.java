2b327fb6ae326ad76fa17277d72bab496bde32f9
2MP2 <mikolajpastuszekk@gmail.com>
2023-01-20 04:13:16 +0100

    problem z pmyslem na listView

--- src/main/java/com/example/cinema/MainCustomerController.java
+++ src/main/java/com/example/cinema/MainCustomerController.java
@@ -1,5 +1,8 @@
 package com.example.cinema;
 
+import Model.Movies;
+import Model.Seances;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,10 +10,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MainCustomerController implements Initializable {
@@ -21,6 +27,11 @@ public class MainCustomerController implements Initializable {
     private Scene scene;
     private Parent root;
 
+    @FXML
+    private Button goToBuyTicket,goBookTicket,goToMyReservations,goToEditCustomer;
+
+    @FXML
+    ListView<Seances> seancesListView;
     public static void setModel(Model model) {
         if(MainCustomerController.model != null)
             throw new IllegalStateException("Model can only be initialized once");
@@ -40,8 +51,92 @@ public class MainCustomerController implements Initializable {
         stage.show();
     }
 
+    @FXML
+    private void goToBuyTicket(ActionEvent actionEvent) throws IOException {
+
+        fxmlLoader = new FXMLLoader(getClass().getResource(""));
+        root = fxmlLoader.load();
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void goBookTicket(ActionEvent actionEvent) throws IOException {
+
+        fxmlLoader = new FXMLLoader(getClass().getResource(""));
+        root = fxmlLoader.load();
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void goToMyReservations(ActionEvent actionEvent) throws IOException {
+
+        fxmlLoader = new FXMLLoader(getClass().getResource(""));
+        root = fxmlLoader.load();
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void goToEditCustomer(ActionEvent actionEvent) throws IOException {
+
+        fxmlLoader = new FXMLLoader(getClass().getResource(""));
+        root = fxmlLoader.load();
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        List<Seances> listS = model.getDatabase().getSeancesList();
+        List<Movies> listM = model.getDatabase().getMoviesList();
+
+        for (Seances s: listS)
+        {
+            String title = "";
+            int length = 0;
+            char dub_sub_lec = 'X';
+            boolean is3D = false;
+
+            for(Movies m:listM)
+            {
+                if(s.getId_movie()==m.getId_movie())
+                {
+                    title = m.getTitle();
+                    length = m.getLength();
+                    dub_sub_lec = m.getDub_sub_lec();
+                    is3D = m.isIs3D();
+                }
+            }
+            String fullString = s.getId_seance() + " " +
+                    s.getStart_time() + " " +
+                    s.getEnd_time() + " " +
+                    s.getTicket_price() + " " +
+                    title + " " + length + " " + dub_sub_lec + " " + is3D + " " +
+                    s.getId_screening_room();
+
+            String full = title + " "+ length + "min "
+                    + Math.round(s.getTicket_price() * 100.00)/100.00 + "zł "
+                    + s.getStart_time().getHours() + ":" + ((s.getStart_time().getMinutes()<10) ? "0" : "") + s.getStart_time().getMinutes() + "-"
+                    + s.getEnd_time().getHours() + ":" + s.getEnd_time().getMinutes()
+                    + ((Character.toUpperCase(dub_sub_lec) == 'D') ? " DUBBING"
+                    : ((Character.toUpperCase(dub_sub_lec) == 'S') ? " NAPISY"
+                    : ((Character.toUpperCase(dub_sub_lec) == 'L') ? " LECTOR" : " XXX")))
+                    + ((is3D) ? " 3D" : " 2D");
+
+
+            seancesListView.getItems().add(full);
+        }
 
     }
 
